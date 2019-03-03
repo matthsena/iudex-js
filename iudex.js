e521f15449afa112c57fce66b2150c5edb945ff8
Matheus <matheusalexandre.sena99@gmail.com>
2019-03-02 23:50:25 -0300

    Created IudexBrowser class to handle Browser info

--- iudex.js
+++ iudex.js
@@ -11,6 +11,7 @@
 // GLOBAL VARIABLES
 
 let date;
+let browser;
 
 document.addEventListener('readystatechange', () => {
 
@@ -18,9 +19,62 @@ document.addEventListener('readystatechange', () => {
 
         date = new IudexDate();
         console.log(date.getDate())
+        browser = new IudexBrowser();
+        console.log(browser.getBrowser());
     }
 });
 
+// Class to handle browser info 
+class IudexBrowser {
+    constructor() {
+
+        // LOCAL VARIABLES
+        this.userAgent = window.navigator.userAgent;
+        this.language = window.navigator.language;
+        this.vendor = window.navigator.vendor;
+        this.browser;
+    }
+
+    setBrowser() {
+
+        if (this.userAgent.indexOf("Firefox") > -1) {
+
+            this.browser = "Mozilla Firefox";
+        } else if (this.userAgent.indexOf("Opera") > -1 || this.userAgent.indexOf("OPR") > -1) {
+
+            this.browser = "Opera";
+        } else if (this.userAgent.indexOf("FBAN") > -1 || this.userAgent.indexOf("FBAV") > -1) {
+
+            this.browser = "Facebook Web View";
+        } else if (this.userAgent.indexOf("Trident") > -1) {
+
+            this.browser = "Microsoft Internet Explorer";
+        } else if (this.userAgent.indexOf("Edge") > -1) {
+
+            this.browser = "Microsoft Edge";
+        } else if (this.userAgent.indexOf("Chrome") > -1) {
+
+            this.browser = "Google Chrome";
+        } else if (this.userAgent.indexOf("Safari") > -1) {
+
+            this.browser = "Apple Safari";
+        } else {
+
+            this.browser = "unknown";
+        }
+    }
+
+    // Get Browser info
+    getBrowser() {
+        this.setBrowser();
+        
+        return {
+            browser: this.browser,
+            language: this.language,
+            userAgent: this.userAgent
+        };
+    }
+}
 
 // Class to handle date
 class IudexDate {
